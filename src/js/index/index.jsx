6658205324c0f729821cddfa70387ac63d2c3b18
fengshiyu <945039036@qq.com>
2017-04-07 09:05:03 +0800

    巩固

--- src/js/index/index.jsx
+++ src/js/index/index.jsx
@@ -8,9 +8,36 @@ import {
 
 const Com1 = () => {
 	return (
-		
+		<div>组件1</div>
 	)
 }
+const Com2 = () => {
+	return (
+		<div>组件2</div>
+	)
+}
+
+const Com3 = () => {
+	return (
+		<div>组件3</div>
+	)
+}
+
+const Like_link = ({ to, ...args_out }) => {
+	console.log(args_out);
+	return (
+		<Route path={to} children={({ ...arg }) => {
+			console.log(arg);
+			return (
+				<div>
+					{arg.match ? "yes" : "no"}<Link to={to} {...args_out}>到主页</Link>
+				</div>
+			)
+		}}></Route>
+	)
+}
+
+
 
 export class index extends React.Component {
 	constructor(props, context) {
@@ -18,19 +45,21 @@ export class index extends React.Component {
 	}
 	render() {
 		return (
-			<div>
-			
-			</div>
+			<Router>
+				<div>
+					<Like_link to="/" name="冯世雨啦啦啦" />
+				</div>
+			</Router>
 		);
 	}
 }
 
 index.defaultProps = {
-	
+
 };
 
 index.propTypes = {
-	
+
 };
 
 export default index;
