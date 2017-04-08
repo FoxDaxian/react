e4a51be63b9b1c66646aef0ddef59cfd9fc73aec
fengshiyu <945039036@qq.com>
2017-04-08 23:43:03 +0800

    官网说的对，理解能力欠缺

--- src/js/component1/component1.jsx
+++ src/js/component1/component1.jsx
@@ -1,68 +1,13 @@
-import React, { PropTypes } from 'react';
-import { Prompt } from "react-router-dom";
-
-
-export class Com1 extends React.Component {
-	constructor(props, context) {
-		super(props, context);
-		this.state = {
-			isBlock: false
-		};
-	}
-
-	componentWillUnmount() {
-		console.log("被解除绑定了");
-	}
-
-	submit = (e) => {
-		e.target.reset()
-		this.setState({
-			isBlock: false
-		})
-		e.preventDefault()
-	}
-
-	render() {
-		const { isBlock } = this.state
-		return (
-			<form onSubmit={this.submit}>
-				<Prompt
-					when={isBlock}
-					message={location => (
-						`你真的要跳转到 ${location.pathname}么？`
-					)}
-				/>
-
-				<p>
-					是否无法跳转? {isBlock ? '好，现在试试再试试点击那些链接' : '可以正常跳转'}
-				</p>
-
-				<p>
-					<input
-						size="50"
-						placeholder="你这里面输入了以后就不能正常跳转了"
-						onChange={event => {
-							this.setState({
-								isBlock: event.target.value.length > 0
-							})
-						}}
-					/>
-				</p>
-
-				<p>
-					<button>提交表单以后就可以正常跳转了</button>
-				</p>
-			</form>
-		);
-	}
+import React, { Component } from 'react';
+import css from "./component1.scss";
+class Com1 extends Component {
+    render() {
+        return (
+            <div className={css.wrap}>
+                <h3>组件1</h3>
+            </div>
+        );
+    }
 }
-
-Com1.defaultProps = {
-
-};
-
-Com1.propTypes = {
-
-};
 
 export default Com1;
--- src/js/index/index.jsx
+++ src/js/index/index.jsx
@@ -1,66 +1,76 @@
-import React, { PropTypes } from 'react';
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import {
 	BrowserRouter as Router,
 	Route,
 	Link,
-	Switch,
 	Redirect
-} from "react-router-dom";
+} from 'react-router-dom'
+import Com1 from "../component1/component1.jsx";
+import Com2 from "../component2/component2.jsx";
+import Com3 from "../component3/component3.jsx";
+import css from "./index.scss";
 
-const Home = () => (
-	<p>
-		<code> &lt;Switch> </code>会渲染它里面的第一个可以匹配的
-    <code> &lt;Route> </code>，而且一个没有<code> path </code>的
-    <code> &lt;Route> </code> 会满足任何匹配。
-	</p>
-)
+const routes = [{
+	name: "主页",
+	path: "/",
+	exact: "true",
+	com: Com1
+}, {
+	name: "组件2",
+	path: "/Com2",
+	com: Com2
+}, {
+	name: "组件3",
+	path: "/Com3",
+	com: Com3
+}]
 
-const WillMatch = () => <h3>匹配到了！</h3>
-
-export class Index extends React.Component {
-	constructor(props, context) {
-		super(props, context);
-	}
-
+class Index extends Component {
 	render() {
+
 		return (
 			<Router>
-				<div>
-					<div className="links">
-						<div>
-							<Link to="/">主页</Link>
-						</div>
-						<div>
-							<Link to="/old-match">可以啊</Link>
-						</div>
-						<div>
-							<Link to="/will-match">可以啊</Link>
-						</div>
-						<div>
-							<Link to="/will-not-match">不可以</Link>
-						</div>
-						<div>
-							<Link to="/also/will/not/match">不可以</Link>
-						</div>
-					</div>
-					<Switch>
-						<Route path="/" exact component={() => <h1>主页</h1>} />
-						<Redirect from="/old-match" to="/will-match" />
-						<Route path="/will-match" component={WillMatch} />
-						<Route render={ () => <h3>我是备胎</h3>} />
-					</Switch>
+				<div className={css.wrap}>
+					{routes.map((el, i) => {
+						return (
+							<Link key={el.path} to={el.path}>{el.name}</Link>
+						)
+					})}
+
+					{
+						routes.map((el, i) => {
+							return (
+								// 必须有location，用来启动动画，因为react-router需要知道"来龙去脉"
+								<Route key={i} render={({ location }) => {
+									return (
+										<ReactCSSTransitionGroup
+											transitionName="example"
+											transitionEnterTimeout={500}
+											transitionLeaveTimeout={300}
+										>
+
+											<Route
+												exact={!!el.exact ? true : false}
+												location={location}//这个也是必须这样写的
+												key={location.key}//这个也是必须这样写的
+												path={el.path}
+												component={el.com}
+											/>
+
+
+										</ReactCSSTransitionGroup>
+									)
+								}} />
+							)
+						})
+					}
+
+
 				</div>
 			</Router>
 		);
 	}
 }
-
-Index.defaultProps = {
-
-};
-
-Index.propTypes = {
-
-};
 
 export default Index;
