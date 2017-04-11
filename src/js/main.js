411cd8da61b394a3b8b6c4203b53bc11890c1808
fengshiyu <945039036@qq.com>
2017-04-11 13:17:16 +0800

    2017-4-11

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,48 @@
-// import css from "../static/reset.css";
-
-
 import React from 'react';
 import { render } from 'react-dom'
 
-import Index from './index/index.jsx'
-import { createStore } from 'redux'
-import counter from './reducers'
-
-const store = createStore(counter);
 
 
-const test = () => render(
-	<Index />,
+import { createStore, applyMiddleware } from "redux";
+
+//中间处理者
+const reducer = (state = 0, action) => {
+	let result = state
+	switch (action.type) {
+		case "TO_LOW":
+			result = state - 1;
+			break;
+		case "TO_UP":
+			result = state + 1;
+			break;
+	}
+	return result
+}
+
+
+//初始化redux的store中的数据
+const store = createStore(reducer)
+
+
+//监听创建好的store，每次事件触发，进行回调
+store.subscribe(() => {
+	console.log(store.getState())
+})
+
+//分发动作，触发中间处理者，然后触发订阅
+store.dispatch({ type: 'TO_LOW'})
+store.dispatch({ type: 'TO_LOW'})
+store.dispatch({ type: 'TO_LOW'})
+store.dispatch({ type: 'TO_UP'})
+
+
+
+
+
+render(
+	<div>你好</div>,
 	document.getElementById('app')
 )
-store.subscribe(test);
+
 
 
