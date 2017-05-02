4154900839f2274ed50ac574b92c0727af849f8b
fengshiyu <945039036@qq.com>
2017-05-02 14:58:12 +0800

    开发环境取消提取css

--- config/webpack.build.js
+++ config/webpack.build.js
@@ -6,6 +6,17 @@ const base_config = require("./base.js");
 const webpack = require('webpack');
 const path = require('path');
 
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");//提取出css生成css文件
+
+//因为从js中分离出css的话，被分离出的js 和 分离出的css的 hash一样，以js为准，所以使用contenthash，[contenthash] 是 extract-text-webpack-plugin提供的另一种hash值，意为：文本内容的hash值，用来区分js文件的hash值
+const css_extract = new ExtractTextPlugin({
+	filename: "static/css/css.[contenthash:5].css"
+});
+const scss_extract = new ExtractTextPlugin({
+	filename: "static/css/scss.[contenthash:5].css"
+});
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');//生成html
 
 
@@ -19,11 +30,66 @@ let config = webpackMerge(base_config, {
 		chunkFilename: 'static/js/[name].[chunkhash:5].chunk.js'//为了按需加载组件
 	},
 	module: {
-		rules: [
-			{
-				
-			}
-		]
+		rules: [{//处理css不带css-module
+			test: /\.css$/,
+			include: [path.resolve(__dirname, "../src/static/")],
+			use: css_extract.extract({
+				fallback: "style-loader",
+				use: [{
+					loader: "css-loader"
+				}, {
+					loader: "postcss-loader"
+				}]
+			})
+		}, {//处理scss不带css-module
+			test: /\.scss$/,
+			include: [path.resolve(__dirname, "../src/static/")],
+			use: scss_extract.extract({
+				fallback: "style-loader",
+				use: [{
+					loader: "css-loader"
+				}, {
+					loader: "postcss-loader"
+				}, {
+					loader: "sass-loader"
+				}]
+			})
+		}, {//处理css
+			test: /\.css$/,
+			exclude: [path.resolve(__dirname, "../src/static/")],//根据优先级
+			use: css_extract.extract({
+				fallback: "style-loader",
+				use: [{
+					loader: "css-loader",
+					//是否需要一个全局默认样式或者一个自己自定义的样式？
+					//如果需要的话，就要exclude出去，因为该插件会扩展类名
+					//所以只要有类名就会进行转换，就不起作用了
+					options: {//模块化css
+						modules: true,
+						localIdentName: '[path][name]__[local]--[hash:base64:5]'
+					}
+				}, {
+					loader: "postcss-loader"
+				}]
+			})
+		}, {//处理scss
+			test: /\.scss$/,
+			exclude: [path.resolve(__dirname, "../src/static/")],//根据优先级
+			use: scss_extract.extract({
+				fallback: "style-loader",
+				use: [{
+					loader: "css-loader",
+					options: {//模块化css
+						modules: true,
+						localIdentName: '[path][name]__[local]--[hash:base64:5]'
+					}
+				}, {
+					loader: "postcss-loader"
+				}, {
+					loader: "sass-loader"
+				}]
+			})
+		}]
 	},
 	plugins: [
 		//分离第三方插件库，因为第三方插件库总是不变的，所以让浏览器缓存他，提高速度
@@ -60,7 +126,8 @@ let config = webpackMerge(base_config, {
 				minifyCSS: true,
 				minifyJS: true
 			}
-		})
+		}),
+		css_extract, scss_extract
 	]
 });
 module.exports = config;
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,37 +7,88 @@ const webpackMerge = require('webpack-merge');
 const base_config = require("./base.js");
 
 
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');//生成html
 
 process.noDeprecation = true; //去除一个现在无关紧要的警告
 
 //把入口文件改为数组，并添加上iddleware/client?noInfo=true&reload=true，否则webpack-hot-middleware不会起作用
 const devClient = "./config/dev-client.js";//使用个人设置的配置文件
-for( let key in base_config.entry ){
+for (let key in base_config.entry) {
 	base_config.entry[key] = [devClient].concat(base_config.entry[key]);
 }
 
 
-const config =  webpackMerge(base_config, {
- 	//决定sourcemap文件的模式
- 	devtool: "eval-source-map",
- 	module:{
- 		rules:[
-
- 		]
- 	},
- 	plugins: [
- 		// 开启全局的模块热替换(HMR)
- 		new webpack.HotModuleReplacementPlugin(),
- 		new webpack.NoEmitOnErrorsPlugin(),
- 		//需要有一个html文件指向你的生成的dist，所以使用HtmlWebpackPlugin
- 		//html插件，本地测试模式下，不压缩，打包快
- 		new HtmlWebpackPlugin({
- 			template:path.resolve(__dirname,"../index.html"),
- 			title: "webpack-native",
- 			filename: 'index.html',
- 			inject:true,
- 		}),
- 		]
- 	});
+const config = webpackMerge(base_config, {
+	//决定sourcemap文件的模式
+	devtool: "eval-source-map",
+	module: {
+		rules: [{//处理css不带css-module
+			test: /\.css$/,
+			include: [path.resolve(__dirname, "../src/static/")],
+			use: [{
+				loader: 'style-loader'
+			}, {
+				loader: 'css-loader'
+			}, {
+				loader: "postcss-loader"
+			}]
+		}, {//处理scss不带css-module
+			test: /\.scss$/,
+			include: [path.resolve(__dirname, "../src/static/")],
+			use: [{
+				loader: 'style-loader'
+			}, {
+				loader: 'css-loader'
+			}, {
+				loader: "postcss-loader"
+			}, {
+				loader: "sass-loader"
+			}]
+		}, {//处理css
+			test: /\.css$/,
+			exclude: [path.resolve(__dirname, "../src/static/")],//根据优先级
+			use: [{
+				loader: 'style-loader'
+			}, {
+				loader: 'css-loader',
+				options: {//模块化css
+					modules: true,
+					localIdentName: '[path][name]__[local]--[hash:base64:5]'
+				}
+			}, {
+				loader: "postcss-loader"
+			}]
+		}, {//处理scss
+			test: /\.scss$/,
+			exclude: [path.resolve(__dirname, "../src/static/")],//根据优先级
+			use: [{
+				loader: 'style-loader'
+			}, {
+				loader: 'css-loader',
+				options: {//模块化css
+					modules: true,
+					localIdentName: '[path][name]__[local]--[hash:base64:5]'
+				}
+			}, {
+				loader: "postcss-loader"
+			}, {
+				loader: "sass-loader"
+			}]
+		}]
+	},
+	plugins: [
+		// 开启全局的模块热替换(HMR)
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
+		//需要有一个html文件指向你的生成的dist，所以使用HtmlWebpackPlugin
+		//html插件，本地测试模式下，不压缩，打包快
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "../index.html"),
+			title: "webpack-native",
+			filename: 'index.html',
+			inject: true,
+		})
+	]
+});
 module.exports = config;
